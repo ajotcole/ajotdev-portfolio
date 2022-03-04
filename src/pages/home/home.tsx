69bdae56c636266975d43f9eb0155de25cafc7b5
Alexander James Cole <aj@co-le.com>
2022-03-04 13:24:23 +0100

    Add basic layout

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,3 +1,14 @@
+import styles from "./home.module.scss";
+
 export const Home = () => {
-  return <div>I am home</div>;
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.innerContent}>
+        <div className={styles.headline}>AJOT.DEV</div>
+        <div>Image and Text goes here</div>
+        <div>Welcome Text goes here</div>
+        <div>CV goes here</div>
+      </div>
+    </div>
+  );
 };
